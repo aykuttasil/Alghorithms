90232ffdc4f4949034d007425de7b3c9ffa6c726
Aykut Asil <aykuttasil@gmail.com>
2018-10-22 00:37:16 +0300

    two-sum

---
+++ two-sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace two_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 2, 3, 4, 7 };
+            var x = TwoSum(arr, 5);
+
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var x = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var y = nums[j];
+                    if (x + y == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+
+}
