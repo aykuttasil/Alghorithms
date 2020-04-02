4579fd8f3e330d1c41b0d8823e4c5cf12d9a1ff6
Aykut Asil <aykuttasil@gmail.com>
2020-04-02 16:31:17 +0300

    happy number refactoring

--- happy-number/Program.cs
+++ happy-number/Program.cs
@@ -1,4 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, 
+replace the number by the sum of the squares of its digits, 
+and repeat the process until the number equals 1 (where it will stay), 
+or it loops endlessly in a cycle which does not include 1. 
+Those numbers for which this process ends in 1 are happy numbers.
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
 
 namespace happy_number
 {
@@ -6,31 +25,62 @@ namespace happy_number
     {
         static void Main(string[] args)
         {
-            int result = 6;
+            int num = 1111111;
+            //Console.WriteLine(num + " % 10 =" + num % 10);
+            //Console.WriteLine("result:" + sumOfSquareOfDigits(num));
 
-            while (result != 1 && result != 4)
+            HashSet<int> hash = new HashSet<int>();
+            bool isOK = true;
+            while (num != 1)
             {
-                result = sumOfSquareOfDigits(result);
-                Console.WriteLine("Result:" + result);
+                num = sumOfSquareOfDigits(num);
+                if (hash.Contains(num))
+                {
+                    isOK = false;
+                    break;
+                }
+                else
+                {
+                    hash.Add(num);
+                }
+
+                if (num == 1)
+                {
+                    isOK = true;
+                    break;
+                }
             }
 
-            if (result == 1)
-                Console.WriteLine("Happy number");
-            else if (result == 4)
-                Console.WriteLine("Not Happy number");
+            if (isOK)
+            {
+                Console.WriteLine(num + " is Happy Number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not Happy Number");
+            }
+
+
         }
 
+        /*
+            12 = 1^2 + 2^2 = 5
+            5 = 5^2 = 25
+            25 = 2'2 + 5^2 = 29
+            29 = 2^2 + 9 ^ 2 = 85
+            ..            
+        */
         public static int sumOfSquareOfDigits(int num)
         {
-            int rem = 0, sum = 0;
-
-            //Calculates the sum of squares of digits  
-            while (num > 0)
+            int sum = 0;
+            int x = 0;
+            while (num != 0)
             {
-                rem = num % 10;
-                sum = sum + (rem * rem);
+                x = (num % 10) * (num % 10);
+                sum += x;
                 num = num / 10;
             }
+            Console.WriteLine("sum:" + sum);
             return sum;
         }
     }
