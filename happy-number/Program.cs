c9e628a216a3274809f65ae419bfc2fe9878534b
Aykut Asil <aykuttasil@gmail.com>
2020-04-02 12:23:41 +0300

    happy number

---
+++ happy-number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace happy_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(""+sumOfSquareOfDigits(12));
+        }
+
+        public static int sumOfSquareOfDigits(int a)
+        {
+            var strInt = a.ToString();    
+            var sum = 0;
+            for (int i = 0; i < strInt.Length; i++)
+            {
+                var digit = int.Parse(strInt[i].ToString());
+                sum += digit * digit;            
+            }
+            return sum;
+        }
+    }
+}
