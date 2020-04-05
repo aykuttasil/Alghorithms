3d4a7b9c15524e2c05f630bdded9ead2ff68f4e8
Aykut Asil <aykuttasil@gmail.com>
2020-04-05 23:04:52 +0300

    move zeroes

---
+++ maximum-subarray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) 
+which has the largest sum and return its sum.
+
+Example:
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Follow up:
+If you have figured out the O(n) solution, 
+try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+
+/*
+    [-1,2,-3,1] -> [2]
+    [ 2,3,-5,3] -> [2,3]
+    [-2,1,-3,4,-1,2,1,-5,4,15] -> [4,-1,2,1,-5,4,15] = 20
+*/
+namespace maximum_subarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[4] { -1, 2, -3, 1 };
+
+
+        }
+
+        public static int MaxSubArray(int[] a)
+        {
+            int max_so_far = a[0];
+            int curr_max = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                curr_max = Math.Max(a[i], curr_max + a[i]);
+                max_so_far = Math.Max(max_so_far, curr_max);
+            }
+
+            return max_so_far;
+        }
+    }
+}
---
+++ move-zeroes/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+/*
+- Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+*/
+namespace move_zeroes
+{
+    class Program
+    {
+        static Stopwatch watch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1, 0, 3, 12, 0, 2, 3, 5, 12 };
+            watch.Reset();
+            MoveZeroes1(nums);
+            watch.Reset();
+            MoveZeroes2(nums);
+            watch.Reset();
+            MoveZeroes3(nums);
+        }
+
+
+        // Method 1
+        public static void MoveZeroes1(int[] nums)
+        {
+
+            watch.Start();
+            int countNonZero = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[countNonZero++] = nums[i];
+                }
+            }
+
+            while (countNonZero < nums.Length)
+            {
+                nums[countNonZero++] = 0;
+            }
+            watch.Stop();
+            printScreen(nums);
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+        }
+
+        // Method 2
+        public static void MoveZeroes2(int[] nums)
+        {
+            watch.Start();
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[index++] = nums[i];
+                }
+            }
+
+
+            for (int i = index; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+            watch.Stop();
+            printScreen(nums);
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+
+        // Method 3
+        public static void MoveZeroes3(int[] nums)
+        {
+            watch.Start();
+            List<int> list = new List<int>();
+            int zeroCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    list.Add(nums[i]);
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+
+            for (int i = 0; i < zeroCount; i++)
+            {
+                list.Add(0);
+            }
+            watch.Stop();
+            printScreen(nums);
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        public static void printScreen(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine("i:" + nums[i]);
+            }
+        }
+
+    }
+}
