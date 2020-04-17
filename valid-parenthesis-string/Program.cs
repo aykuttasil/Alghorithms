f8e298462da22edd340ac264ae03d180ddc5e780
Aykut Asil <aykuttasil@gmail.com>
2020-04-17 20:48:43 +0300

    update

--- valid-parenthesis-string/Program.cs
+++ valid-parenthesis-string/Program.cs
@@ -33,28 +33,37 @@ namespace valid_parenthesis_string
 
         public static bool CheckValidString(string s)
         {
-           int low = 0;
-        int high = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                low++;
-                high++;
-            } else if (s.charAt(i) == ')') {
-                if (low > 0) {
-                    low--;
+            int low = 0;
+            int high = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    low++;
+                    high++;
                 }
-                high--;
-            } else {
-                if (low > 0) {
-                    low--;
+                else if (s[i] == ')')
+                {
+                    if (low > 0)
+                    {
+                        low--;
+                    }
+                    high--;
                 }
-                high++;
+                else
+                {
+                    if (low > 0)
+                    {
+                        low--;
+                    }
+                    high++;
+                }
+                if (high < 0)
+                {
+                    return false;
+                }
             }
-            if (high < 0) {
-                return false;
-            }
-        }
-        return low == 0;
+            return low == 0;
         }
     }
 }
