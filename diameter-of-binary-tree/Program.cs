d0ff153232b6c57d4762b962413f8119f0af78fd
Aykut Asil <aykuttasil@gmail.com>
2020-04-19 23:11:56 +0300

    update

--- diameter-of-binary-tree/Program.cs
+++ diameter-of-binary-tree/Program.cs
@@ -32,12 +32,28 @@ namespace diameter_of_binary_tree
     {
         static void Main(string[] args)
         {
-            
+
         }
+
+
+        int max = 0;
 
         public int DiameterOfBinaryTree(TreeNode root)
         {
-            
+            maxDepth(root);
+            return max;
+        }
+
+        private int maxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+
+            max = Math.Max(max, left + right);
+
+            return Math.Max(left, right) + 1;
         }
 
         public class TreeNode
