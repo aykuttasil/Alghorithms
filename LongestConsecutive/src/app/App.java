161eb693a31816756cf5e32ce7fed790dbecd497
Aykut Asil <aykuttasil@gmail.com>
2020-04-25 01:42:58 +0300

    updates

---
+++ LongestConsecutive/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+
+    
+    public int longestConsecutive(final List<Integer> A) {
+        Arrays.sort(A.toArray());
+        
+        return 1;
+    }
+}
