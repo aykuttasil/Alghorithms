12c42d08b46635b35defe4053c6fb1e60d62bd5b
Aykut Asil <aykuttasil@gmail.com>
2020-04-25 01:27:23 +0300

    updates

---
+++ prime-number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("" + isPrime(67));
+        }
+
+        private static bool isPrime(int x)
+        {
+            if (x < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ unique-email-addresses/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace unique_email_addresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new String[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" };
+            var sonuc = NumUniqueEmails(arr);
+            Console.WriteLine("Sonuc: " + sonuc);
+
+        }
+
+        public static int NumUniqueEmails(string[] emails)
+        {
+            var map = new HashSet<String>();
+            for (int i = 0; i < emails.Length; i++)
+            {
+                var email = emails[i];
+                var parts = email.Split("@");
+                var name = parts[0].Replace(".", "");
+                if (name.Contains("+"))
+                {
+                    name = name.Split("+")[0];
+                }
+                var domain = parts[1];
+                map.Add(name + "@" + domain);
+            }
+            return map.Count;
+        }
+
+    }
+}
